ea9c7e9903427ce7820973f2790077e01d381599
liyanming <13315100614@163.com>
2020-08-16 18:09:10 +0800

    增加使用栈进行求解优化，时间换空间

--- src/main/java/com/li/stack/Test2.java
+++ src/main/java/com/li/stack/Test2.java
@@ -1,5 +1,7 @@
 package com.li.stack;
 
+import java.util.Stack;
+
 /**
  * 对应力扣84题
  *
@@ -8,9 +10,17 @@ package com.li.stack;
 public class Test2 {
     public static void main(String[] args) {
         int[] maxArea = new int[]{2, 1, 5, 6, 2, 3};
-        System.out.println(Test2.largestRectangleArea(maxArea));
+//        System.out.println(Test2.largestRectangleArea(maxArea));
+        System.out.println(Test2.largestRectangleStackArea(maxArea));
+
     }
 
+    /**
+     * 暴力求解
+     *
+     * @param heights
+     * @return
+     */
     public static int largestRectangleArea(int[] heights) {
         int area = 0;
         for (int i = 0; i < heights.length; i++) {
@@ -31,5 +41,33 @@ public class Test2 {
 
         }
         return area;
+    }
+
+    /**
+     * 使用栈进行求解
+     *
+     * @param heights
+     * @return
+     */
+    public static int largestRectangleStackArea(int[] heights) {
+        Stack<Integer> ints = new Stack<Integer>();
+        ints.push(-1);
+        int maxArea = 0;
+        for (int i = 0; i < heights.length; i++) {
+            while (ints.peek() != -1 && heights[ints.peek()] > heights[i]) {
+                //如果栈顶元素大于即将入栈的元素，栈中的元素进行出栈操作
+                Integer peek = ints.peek();
+                int nextArea = (i - peek) * heights[ints.pop()];
+                maxArea = Math.max(maxArea, nextArea);
+            }
+            ints.push(i);
+        }
+        while (ints.peek() != -1) {
+            Integer pop = ints.pop();
+            int next = (heights.length - ints.peek() - 1) * heights[pop];
+            System.out.println(next);
+            maxArea = Math.max(maxArea,next);
+        }
+        return maxArea;
     }
 }
